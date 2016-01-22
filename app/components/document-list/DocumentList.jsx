4b0e44dc65a10f8ddc536004c2a4407e0e205d28
RM Saksida <rm@saksida.com.br>
2016-01-22 18:17:49 -0200

    Add Share to Classroom button

--- app/components/document-list/DocumentList.jsx
+++ app/components/document-list/DocumentList.jsx
@@ -3,9 +3,14 @@ import Document from '../document';
 import styles from './DocumentList.scss';
 
 export default class DocumentList extends React.Component {
+
+  componentDidUpdate() {
+    gapi.sharetoclassroom.go(this.refs.documentList);
+  }
+
   render() {
     return (
-      <ul className={styles['document-list']}>
+      <ul className={styles['document-list']} ref='documentList'>
         {this.props.documents.map(document => {
           return <Document key={document.id} document={document} />;
         })}
--- app/components/document/Document.jsx
+++ app/components/document/Document.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import styles from './Document.scss';
 
 export default class Document extends React.Component {
+
   render() {
     return (
       <li className={styles.document}>
         <div>
           <a className={styles.title} href={this.props.document.url} target="_blank">
             {this.props.document.title}
+            <div className={styles['g-sharetoclassroom-container']}>
+              <div className="g-sharetoclassroom"
+                data-title={this.props.document.title}
+                data-body={this.props.document.description}
+                data-size="20"
+                data-url={this.props.document.url} />
+            </div>
           </a>
           <p>{this.props.document.description}</p>
         </div>
