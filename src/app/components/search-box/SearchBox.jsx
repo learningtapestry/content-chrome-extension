96632b07bee5da998c95a00d86747ac97cbac1cb
RM Saksida <rm@saksida.com.br>
2016-01-24 09:49:24 -0200

    Debounce search box queries

--- src/app/components/search-box/SearchBox.jsx
+++ src/app/components/search-box/SearchBox.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import bus from '../../bus';
 import styles from './SearchBox.scss';
+import debounce from 'lodash/debounce';
 
 export default class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateQ = debounce(this.updateQ, 200);
+  }
+
   componentDidMount() {
     this.refs.searchBox.focus(); 
   }
 
   onChange(event) {
-    bus.emit('updateQ', event.target.value);
+    event.persist();
+    this.updateQ(event.target.value);
+  }
+
+  updateQ(value) {
+    bus.emit('updateQ', value);
   }
 
   render() {
     return (
       <div className={styles['search-box-wrapper']}>
-        <input ref='searchBox' className={styles['search-box']} placeholder="Search" onChange={this.onChange} />
+        <input ref='searchBox' className={styles['search-box']} placeholder="Search" onChange={this.onChange.bind(this)} />
       </div>
     );
   }
