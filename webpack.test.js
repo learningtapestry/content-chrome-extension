85840757da947257c3470f2e4983f809a4078cb4
RM Saksida <rm@saksida.com.br>
2016-01-28 08:16:36 -0200

    Fix inconsistencies in webpack.config (test)

--- webpack.test.js
+++ webpack.test.js
@@ -1,13 +1,12 @@
-var Webpack = require('webpack');
+var webpack = require('webpack');
 var webpackBase = require('./webpack.base.js');
 var extend = require('lodash').extend;
 var path = require('path');
-
 var envConfig = require('./env.js');
 
 module.exports = extend({}, webpackBase, {
   resolve: {
     root: [path.resolve(__dirname, 'src')]
   },
-  plugins: [new Webpack.DefinePlugin(envConfig)]
+  plugins: [new webpack.DefinePlugin(envConfig)]
 });
