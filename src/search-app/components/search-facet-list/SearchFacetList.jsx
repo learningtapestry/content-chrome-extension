225e84cf37e66bb627bface712ab63b4df0a16e3
RM Saksida <rm@saksida.com.br>
2016-03-26 17:39:06 -0300

    Add logo

--- src/search-app/components/search-facet-list/SearchFacetList.jsx
+++ src/search-app/components/search-facet-list/SearchFacetList.jsx
@@ -4,6 +4,8 @@ import SearchFacetClearer from '../search-facet-clearer';
 import SearchFacetGroup from '../search-facet-group';
 import FACET_GROUPS from '../../facetGroups';
 import styles from './SearchFacetList.scss';
+import googleLogo from '../../images/logo_google_classroom.png';
+import genericLogo from '../../images/logo_generic.png';
 
 export default class SearchFacetList extends React.Component {
   availableGroups() {
@@ -14,20 +16,36 @@ export default class SearchFacetList extends React.Component {
   }
 
   render() {
+    let logo;
+    
+    switch(BUILD) {
+      case 'google':
+        logo = googleLogo;
+        break;
+      case 'generic':
+      default:
+        logo = genericLogo;
+    }
+
     return (
-      <div className={styles['search-facet-list']}>
-        <div className={styles['search-facet-clearer']}>
-          <SearchFacetClearer />
+      <div>
+        <div className={styles['logo']}>
+          <img src={logo} />
         </div>
-        <ul>
-          {this.availableGroups().map(group => {
-            return <SearchFacetGroup
-              key={group}
-              group={group}
-              facets={this.props.groups[group]}
-            />;
-          })}
-        </ul>
+        <div className={styles['search-facet-list']}>
+          <div className={styles['search-facet-clearer']}>
+            <SearchFacetClearer />
+          </div>
+          <ul>
+            {this.availableGroups().map(group => {
+              return <SearchFacetGroup
+                key={group}
+                group={group}
+                facets={this.props.groups[group]}
+              />;
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
