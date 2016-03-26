265e959037bfcb13b034d237ddc32b7c3599f889
RM Saksida <rm@saksida.com.br>
2016-03-26 17:38:33 -0300

    Add BUILD env variable

--- env.js
+++ env.js
@@ -4,5 +4,6 @@ require('dotenv').config();
 module.exports = {
   API_KEY: JSON.stringify(process.env.API_KEY),
   API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+  BUILD: JSON.stringify(process.env.BUILD),
   'global.GENTLY': false
 };
